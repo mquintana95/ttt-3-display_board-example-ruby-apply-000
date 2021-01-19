c03d4f8a1cca3f69fe4cd593acb8462988d501da
Marcus Quintana <mq97325@gmail.com>
2021-01-19 20:49:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 def display_board
@@ -12,3 +13,12 @@ display_board = "A Tic Tac Tow Board"
 
 puts "A Tic Tac Tow Board"
 
+=======
+def display_board 
+  print "   |   |   "
+  print "-----------"
+  print "   |   |   "
+  print "-----------"
+  print "   |   |   "
+  
+>>>>>>> 862679b383a2d9d8317f91c8cfe798707a82b15c
